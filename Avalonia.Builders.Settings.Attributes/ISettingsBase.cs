d04293e5f6de04a47a6e6732689f82b722b2d247
ArchLeaders <archparliament@gmail.com>
2022-10-09 12:55:20 -0700

    Impl settings base interface

---
+++ Avalonia.Builders.Settings.Attributes/ISettingsBase.cs
@@ -0,0 +1,8 @@
+﻿namespace Avalonia.Builders.Settings.Core
+{
+    public interface ISettingsBase
+    {
+        public bool RequiresInput { get; set; }
+        public ISettingsBase Save();
+    }
+}
