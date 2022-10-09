019bc91da0153c1e238ce357f4718815f300e6f8
ArchLeaders <archparliament@gmail.com>
2022-10-09 12:57:06 -0700

    Internal brush converter extension

---
+++ Avalonia.Builders.Settings/Extensions/BrushExt.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Media;
+using System;
+
+namespace Avalonia.Builders.Settings.Extensions
+{
+    internal static class BrushExt
+    {
+        internal static Brush ToBrush(this string color) => new SolidColorBrush(Convert.ToUInt32(color.Replace("#", "").PadLeft(8, 'F'), 16));
+        internal static Brush? ToBrush(this bool? value)
+        {
+            return (value switch {
+                true => SettingsFactoryOptions.ValidateTrueBrush,
+                false => SettingsFactoryOptions.ValidateFalseBrush,
+                _ => SettingsFactoryOptions.ValidateNullBrush,
+            }).ToBrush();
+        }
+    }
+}
