bd47079fba3e02c1e229ea84a08c79fd91176570
ArchLeaders <archparliament@gmail.com>
2022-10-09 13:02:07 -0700

    Built-in settings user interface

---
+++ Avalonia.Builders.Settings/Views/SettingsFactoryView.axaml.cs
@@ -0,0 +1,27 @@
+using Avalonia.Builders.Settings.Core;
+using Avalonia.Builders.Settings.ViewModels;
+using Avalonia.Controls;
+using System;
+
+namespace Avalonia.Builders.Settings.Views
+{
+    public partial class SettingsFactoryView : SettingsFactory
+    {
+        [Obsolete("Only implemented to statisfy the compiler.")]
+        public SettingsFactoryView() { }
+        public SettingsFactoryView(SettingsFactoryViewModel binding, ISettingsValidator validator, ISettingsBase settingsBase)
+        {
+            InitializeComponent();
+
+            // Very much unnecessary, but not having this bothers me.
+            // Allows you to focus seemingly nothing.
+            Grid focusDelegate = this.FindControl<Grid>("FocusDelegate")!;
+            focusDelegate.PointerPressed += (_, _) => focusDelegate.Focus();
+            Grid focusDelegate2 = this.FindControl<Grid>("FocusDelegate2")!;
+            focusDelegate2.PointerPressed += (_, _) => focusDelegate.Focus();
+
+            // Initialize settings factory
+            InitializeSettingsFactory(binding, validator, settingsBase);
+        }
+    }
+}
