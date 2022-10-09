39d1339d63e87d174e04c459db6ba7aa77dbe876
ArchLeaders <archparliament@gmail.com>
2022-10-09 12:58:49 -0700

    UiElement name converter extension

---
+++ Avalonia.Builders.Settings/Extensions/NameExt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avalonia.Builders.Settings.Extensions
+{
+    internal static class NameExt
+    {
+        internal static Dictionary<string, string> InvalidChars { get; set; } = new() {
+            { " ", "_" }
+        };
+
+        internal static string SafeName(this string name)
+        {
+            foreach (var invalidChar in InvalidChars) {
+                name = name.Replace(invalidChar.Key, invalidChar.Value);
+            }
+
+            return name;
+        }
+    }
+}
