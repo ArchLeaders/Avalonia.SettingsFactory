b5b43b0561e00568c4caab32c4ad82ecd17bf652
ArchLeaders <archparliament@gmail.com>
2022-10-09 13:00:56 -0700

    Impl SettingsFactory base ViewModel

---
+++ Avalonia.Builders.Settings/ViewModels/SettingsFactoryViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia.Controls;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avalonia.Builders.Settings.ViewModels
+{
+    public class SettingsFactoryViewModel : ReactiveObject
+    {
+        private StackPanel? activeElement;
+        public StackPanel? ActiveElement {
+            get => activeElement;
+            set => this.RaiseAndSetIfChanged(ref activeElement, value);
+        }
+
+        private bool canCancel;
+        public bool CanCancel {
+            get => canCancel;
+            set => this.RaiseAndSetIfChanged(ref canCancel, value);
+        }
+
+        public SettingsFactoryViewModel(bool canCancel) => this.canCancel = canCancel;
+    }
+}
